6afb468376064a61003aa5c169c5d52e6d9df804
Luzyanin Alexey <261-96@mail.ru>
2019-11-20 17:59:46 +0700

    not working

---
+++ VEGA_JSON/JSON_Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEGA_JSON
+{
+    class Auth
+    {
+        string cmd;
+        string login;
+        string password;
+
+        public Auth(string cmd, string login, string password)
+        {
+            this.cmd = cmd;
+            this.login = login;
+            this.password = password;
+        }
+    }
+}
--- VEGA_JSON/Program.cs
+++ VEGA_JSON/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace VEGA_JSON
 {
@@ -6,7 +10,27 @@ namespace VEGA_JSON
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Auth auth = new Auth("auth_req", "root", "123");
+            string sending = JsonConvert.SerializeObject(auth);
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(sending);
+
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse("192.168.100.7"), 8002);
+
+            NetworkStream networkStream = tcpClient.GetStream();
+            //var task = Task.Factory.StartNew(() => Listen(networkStream));
+            networkStream.Write(data, 0, data.Length);
+
+            Console.WriteLine("SENT");
+            byte[] dataread = new byte[1000];
+            var read = networkStream.Read(dataread, 0, dataread.Length);
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(dataread, 0, read));
+            Console.ReadLine();
+        }
+
+        static void Listen(NetworkStream networkStream)
+        {
+            
         }
     }
 }

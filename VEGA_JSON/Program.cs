0dc823a7b9c4ba210d0e3e675ced39538cd2c32b
Luzyanin Alexey <261-96@mail.ru>
2019-11-20 17:26:00 +0700

    Добавьте файлы проекта.

---
+++ VEGA_JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VEGA_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
